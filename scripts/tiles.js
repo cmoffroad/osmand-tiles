8011bef043afbb76ed5871afe0d9a176512cfc8a
Julien ETIENNE <julien.etienne@intuiquest.com>
2024-07-10 14:09:05 +0700

    update tiles

--- scripts/tiles.js
+++ scripts/tiles.js
@@ -12,30 +12,14 @@ processObfsTiles({
   zooms: [ 13 ]
 });
 
-
 // processObfsTiles({
 //   obfsDir: './data/latest',
 //   obfs: listOBFs('./data/latest'),
 //   renderingName: '../osmand-dirtbike-plugin/src/rendering/dirtbike',
-//   renderingProperties: 'lang=en,hideContour=true,hideWater=true,hideBoundaries=true,hideRailway=true,hidePeaks=true,hidePOIs=true,hideTexts=true,hideLanduse=true,groundSurveyMode=true,detailedColors=true',
-//   renderingBackgroundColor: '#efebe9',
-//   outputDir: './dist/tiles/locator',
-//   center: [ 19.070825827131095,99.03986245393754 ],
-//   tiles: [ 30, 25 ],
+//   renderingProperties: 'lang=en,contourColorScheme=brown,contourDensity=high,contourLines=11,contourWidth=thin,groundSurveyMode=true,detailedColors=true,hideContour=true,hideContourLabels=true,showFixme=true',
+//   outputDir: './dist/tiles',
+//   center: [ 21.721350995105148,104.52409744262697 ],
+//   tiles: [ 10, 10 ],
 //   zooms: [ 13 ]
 // });
 
-
-
-// processSqliteTiles({
-//   inputDir: './data',
-//   inputFilter: (f) => f.match('Hillshade'),
-//   outputDir: `./dist/tiles/hillshade`,
-//   zoom: [ 12 ]
-// });
-
-// processSqliteTiles({
-//   inputFile: findSqliteDB('./data/latest', 'Slope Thailand'),
-//   outputDir: `./dist/tiles/slope`,
-//   zoom: [ 11 ]
-// });
