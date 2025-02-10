7ea74dfade816a463faa0c688f1401ae8d69d704
Julien ETIENNE <julien.etienne@intuiquest.com>
2025-02-10 13:06:31 +0700

    update tiles

--- scripts/tiles.js
+++ scripts/tiles.js
@@ -4,9 +4,21 @@ processObfsTiles({
   obfsDir: './data/latest',
   obfs: listOBFs('./data/latest'),
   renderingName: '../osmand-dirtbike-plugin/src/rendering/dirtbike',
-  renderingProperties: 'lang=en,contourColorScheme=brown,contourDensity=high,contourLines=11,contourWidth=thin,groundSurveyMode=false,showUnknown=false,hideContour=false,hideContourLabels=true,hideBoundaries=true,showFixme=false',
+  renderingProperties: 'lang=en,contourColorScheme=brown,contourDensity=high,contourLines=11,contourWidth=thin,groundSurveyMode=false,showUnknown=false,hideContour=false,hideContourLabels=true,hideBoundaries=true,showFixme=false,hideMinorRoads=false,hideDetailedColors=false',
   // renderingBackgroundColor: '#EEEDDF',
   outputDir: './dist/tiles',
+  center: [ 19.070825827131095,99.03986245393754 ],
+  tiles: [ 30, 25 ],
+  zooms: [ 13 ]
+});
+
+processObfsTiles({
+  obfsDir: './data/latest',
+  obfs: listOBFs('./data/latest'),
+  renderingName: '../osmand-dirtbike-plugin/src/rendering/dirtbike',
+  renderingProperties: 'lang=en,contourColorScheme=brown,contourDensity=high,contourLines=11,contourWidth=thin,groundSurveyMode=false,showUnknown=false,hideContour=true,hideContourLabels=true,hideBoundaries=true,showFixme=false,hideMinorRoads=true,hideDetailedColors=true',
+  renderingBackgroundColor: '#EEEDDF',
+  outputDir: './dist/tiles/major',
   center: [ 19.070825827131095,99.03986245393754 ],
   tiles: [ 30, 25 ],
   zooms: [ 13 ]
